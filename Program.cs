b0b63c712fc03ec66ad0cbca262c9f08b2495ccb
vk2026 <vladk.2026@mtchs.org>
2025-08-29 22:06:07 -0600

    v1.0

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ using System.Text.Json;
 internal class Program
 {
   static int PlayerWins = 0;
-  static int ComputerWins = 0;
+  static int OpponentWins = 0;
   static int MatchesPlayed = 0;
+  static bool? isMultiplayer = null;
 
   static readonly Dictionary<string, string?> beats = new()  {
       { "rock", "scissors" },
@@ -15,7 +16,7 @@ internal class Program
       { "dynamite", null },
   };
 
-  static Dictionary<string, int> PlayerChoiceCounts = new()  {
+  static readonly Dictionary<string, int> PlayerChoiceCounts = new()  {
       { "rock", 0 },
       { "paper", 0 },
       { "scissors", 0 },
@@ -26,58 +27,118 @@ internal class Program
 
   private static void Main()
   {
-    LoadGame();
+    Console.Clear();
+
+    if (isMultiplayer == null) isMultiplayer = MultiPlayer();
+
+    if ((bool)isMultiplayer)
+    {
+      PlayerWins = 0;
+      OpponentWins = 0;
+      MatchesPlayed = 0;
+    }
+    else
+    {
+      LoadGame();
+    }
 
     Console.WriteLine("Rock, Paper, Scissors");
 
-    string userHand = ChooseHand();
-    string computerHand = GetComputerHand();
+    string playerHand = ChooseHand();
+    string opponentHand = (bool)isMultiplayer ? ChooseHand() : GetComputerHand();
 
     bool? playerWon = null;
 
-    Console.WriteLine($"You chose {userHand}");
-    Console.WriteLine($"Computer chose {computerHand}");
+    Console.WriteLine($"You chose {playerHand}");
+    Console.WriteLine($"Opponent chose {opponentHand}");
 
-    if (beats[userHand] == null)
+    if (playerHand == "dynamite")
     {
-      playerWon = true;
+      playerWon = beats[opponentHand] != null;
       PlayerChoiceCounts["dynamite"]++;
     }
-    else if (beats[computerHand] == null) playerWon = false;
-    else if (userHand == "scissors")
+    else if (playerHand == "scissors")
     {
-      playerWon = computerHand == beats[userHand];
+      playerWon = opponentHand == beats[playerHand];
       PlayerChoiceCounts["scissors"]++;
     }
-    else if (userHand == "paper")
+    else if (playerHand == "paper")
     {
-      playerWon = computerHand == beats[userHand];
+      playerWon = opponentHand == beats[playerHand];
       PlayerChoiceCounts["paper"]++;
     }
-    else if (userHand == "rock")
+    else if (playerHand == "rock")
     {
-      playerWon = computerHand == beats[userHand];
+      playerWon = opponentHand == beats[playerHand];
       PlayerChoiceCounts["rock"]++;
     }
 
-    if (playerWon == null)
-    {
-      Console.WriteLine("TIE");
-    }
+    if (playerWon == null) Console.WriteLine("TIE");
     else if ((bool)playerWon)
     {
-      Console.WriteLine("You WIN");
+      Console.WriteLine("Player WIN");
       PlayerWins++;
     }
     else
     {
-      Console.WriteLine("Computer WIN");
-      ComputerWins++;
+      Console.WriteLine("Opponent WIN");
+      OpponentWins++;
     }
 
     MatchesPlayed++;
-    SaveGame();
-    playAgain();
+
+    if (!(bool)isMultiplayer)
+    {
+      SaveGame();
+    }
+
+    PlayAgain();
+  }
+
+  static bool MultiPlayer()
+  {
+    int selectedIndex = 0;
+    bool output = false;
+
+    ConsoleKey key;
+
+    List<string> choices = [
+      "1/c. Player vs Computer",
+      "2/p. Player vs Player2 (note: stats are not saved)",
+    ];
+
+    do
+    {
+      Console.Clear();
+
+      Console.WriteLine("Choose a Game Mode");
+      Console.WriteLine("");
+
+      for (int i = 0; i < choices.Count; i++)
+      {
+        if (i == selectedIndex)
+        {
+          Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        Console.WriteLine(choices[i]);
+        Console.ResetColor();
+      }
+
+      Console.WriteLine("");
+      Console.WriteLine("Type SYMBOL or use ARROW KEYS to select and ENTER: ");
+
+      key = Console.ReadKey(true).Key;
+
+      if (key == ConsoleKey.Enter) output = selectedIndex == 1;
+      else if (key == ConsoleKey.UpArrow && selectedIndex != 0) selectedIndex--;
+      else if (key == ConsoleKey.DownArrow && selectedIndex != choices.Count - 1) selectedIndex++;
+      else if (key is ConsoleKey.D1 or ConsoleKey.C) selectedIndex = 0;
+      else if (key is ConsoleKey.D2 or ConsoleKey.P) selectedIndex = 1;
+    }
+    while (key != ConsoleKey.Enter);
+
+    return output;
   }
 
   static string ChooseHand()
@@ -117,7 +178,7 @@ internal class Program
 
       if (key is ConsoleKey.Enter) output = hands[selectedIndex];
       else if (key is ConsoleKey.UpArrow && selectedIndex != 0) selectedIndex--;
-      else if (key is ConsoleKey.DownArrow && selectedIndex != hands.Count - 1) selectedIndex++;
+      else if (key is ConsoleKey.DownArrow && selectedIndex != choices.Count - 1) selectedIndex++;
       else if (key is ConsoleKey.D1 or ConsoleKey.R) selectedIndex = 0;
       else if (key is ConsoleKey.D2 or ConsoleKey.P) selectedIndex = 1;
       else if (key is ConsoleKey.D3 or ConsoleKey.S) selectedIndex = 2;
@@ -130,35 +191,34 @@ internal class Program
 
   static string GetComputerHand()
   {
-    string maxChoice = PlayerChoiceCounts.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-    Console.WriteLine(PlayerChoiceCounts[maxChoice]);
+    string? maxChoice = PlayerChoiceCounts.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
 
-    var matchingKeys = PlayerChoiceCounts.Where(kvp => kvp.Value == PlayerChoiceCounts[maxChoice])
-      .Select(kvp => kvp.Key)
-      .ToList();
+    var matchingCounts = PlayerChoiceCounts.Where(kvp => kvp.Value == PlayerChoiceCounts[maxChoice]).Select(kvp => kvp.Key).ToList();
 
     Random rand = new();
     string randomHand = hands[rand.Next(0, hands.Count)];
 
-    if (matchingKeys.Count == 2)
-    {
-      return matchingKeys[rand.Next(0, 1)];
-    }
-    else if (matchingKeys.Count == 3) return randomHand;
+    if (matchingCounts.Count == 2)
+      return matchingCounts[rand.Next(0, 1)];
+    else if (matchingCounts.Count == 3)
+      return randomHand;
 
-    return rand.NextDouble() < 0.5 ? beats[maxChoice] : randomHand;
-    // returning value of dynamite breaks it
+    double countAverage = PlayerChoiceCounts.Values.Average();
+    double chance = PlayerChoiceCounts[maxChoice] > countAverage ? 1.00 : rand.NextDouble();
+    string chosenHand = beats[maxChoice] ?? "dynamite";
+
+    return chance < 0.7 ? chosenHand : randomHand;
   }
 
-  static void playAgain()
+  static void PlayAgain()
   {
     string? userInput;
 
     do
     {
-      Console.WriteLine($"\nYou have played {MatchesPlayed} matches\n");
-      Console.WriteLine($"You have won {PlayerWins} matches. ({Math.Round((double)PlayerWins / MatchesPlayed * 100, 2)}%)\n");
-      Console.WriteLine($"Computer has won {ComputerWins} matches. ({Math.Round((double)ComputerWins / MatchesPlayed * 100, 2)}%)\n");
+      Console.WriteLine($"\n{MatchesPlayed} matches played\n");
+      Console.WriteLine($"Player has won {PlayerWins} matches. ({Math.Round((double)PlayerWins / MatchesPlayed * 100, 2)}%)\n");
+      Console.WriteLine($"Opponent has won {OpponentWins} matches. ({Math.Round((double)OpponentWins / MatchesPlayed * 100, 2)}%)\n");
 
       Console.Write("Play again? (y/n) ");
 
@@ -169,8 +229,7 @@ internal class Program
         Main();
         return;
       }
-
-      if (userInput == "n")
+      else if (userInput == "n")
       {
         Console.WriteLine("Goodbye");
         Environment.Exit(0);
@@ -182,7 +241,7 @@ internal class Program
 
   static void SaveGame()
   {
-    SaveData save = new(PlayerWins, ComputerWins, MatchesPlayed);
+    SaveData save = new(PlayerWins, OpponentWins, MatchesPlayed);
     string saveData = JsonSerializer.Serialize(save);
     File.WriteAllText("saveGame.json", saveData);
   }
@@ -196,23 +255,22 @@ internal class Program
     if (data != null)
     {
       PlayerWins = data.PlayerWins;
-      ComputerWins = data.ComputerWins;
+      OpponentWins = data.OpponentWins;
       MatchesPlayed = data.MatchesPlayed;
     }
   }
-
 }
 
 internal class SaveData
 {
   public int PlayerWins { get; set; }
-  public int ComputerWins { get; set; }
+  public int OpponentWins { get; set; }
   public int MatchesPlayed { get; set; }
 
-  public SaveData(int playerWins, int computerWins, int matchesPlayed)
+  public SaveData(int playerWins, int opponentWins, int matchesPlayed)
   {
     PlayerWins = playerWins;
-    ComputerWins = computerWins;
+    OpponentWins = opponentWins;
     MatchesPlayed = matchesPlayed;
   }
 }
